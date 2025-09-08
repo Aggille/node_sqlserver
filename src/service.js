8b407463919d9ceaf312ec108ba151dfbfcbf5c5
Aggille Sistemas de Gestão <39924422+Aggille@users.noreply.github.com>
2025-09-08 07:54:33 -0300

    - deploy em service no windows

---
+++ src/service.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+var Service = require("node-windows").Service;
+
+// Create a new service object
+var svc = new Service({
+  name: "Server Firmma",
+  description: "Servidor de emissão de certificados digitais Firmma.",
+  script: process.env.PATH_CERTIFICADOS + "/src/index.js",
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on("install", function () {
+  svc.start();
+});
+
+svc.install();
