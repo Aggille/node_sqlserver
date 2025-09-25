2ee3358eaff076d26de3dc288490c45958945e5b
Aggille Sistemas de Gestão <39924422+Aggille@users.noreply.github.com>
2025-09-25 12:38:37 -0300

    -

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,6 +4,16 @@ const sql = require("mssql");
 const logger = require("../logger");
 let connection = null;
 
+// Override timezone formatting
+Sequelize.DATE.prototype._stringify = function _stringify(date, options) {
+  date = this._applyTimezone(date, options);
+
+  // Z here means current timezone, _not_ UTC
+  // return date.format('YYYY-MM-DD HH:mm:ss.SSS Z');
+  //return date.format('YYYY-MM-DD HH:mm:ss.SSS');
+  return date.format("YYYY-MM-DD");
+};
+
 const configDB = {
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
